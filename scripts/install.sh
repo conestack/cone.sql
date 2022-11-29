7f6f6d20d0b60e16a8d51e5d50cb7cb131858560
Robert Niederreiter <rnix@squarewave.at>
2022-11-29 17:47:30 +0100

    Tests sane again with master branches

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 if [ -x "$(which python2)" ]; then
     rm -rf py2
 
-    virtualenv --clear -p python2 py2
+    virtualenv -p python2 py2
 
     ./py2/bin/pip install wheel
     ./py2/bin/pip install coverage
@@ -13,18 +13,16 @@ if [ -x "$(which python2)" ]; then
     ./py2/bin/pip install repoze.workflow==0.6.1
     ./py2/bin/pip install https://github.com/conestack/node/archive/master.zip
     ./py2/bin/pip install https://github.com/conestack/node.ext.ugm/archive/master.zip
-    ./py2/bin/pip install https://github.com/conestack/treibstoff/archive/master.zip
     ./py2/bin/pip install https://github.com/conestack/yafowil/archive/master.zip
-    ./py2/bin/pip install https://github.com/conestack/yafowil.bootstrap/archive/2.0.zip
     ./py2/bin/pip install https://github.com/conestack/cone.tile/archive/master.zip
-    ./py2/bin/pip install https://github.com/conestack/cone.app/archive/webresource.zip
-    ./py2/bin/pip install https://github.com/conestack/cone.ugm/archive/webresource.zip
+    ./py2/bin/pip install https://github.com/conestack/cone.app/archive/master.zip
+    ./py2/bin/pip install https://github.com/conestack/cone.ugm/archive/master.zip
     ./py2/bin/pip install -e .[test]
 fi
 if [ -x "$(which python3)" ]; then
     rm -rf py3
 
-    virtualenv --clear -p python3 py3
+    virtualenv -p python3 py3
 
     ./py3/bin/pip install wheel
     ./py3/bin/pip install coverage
@@ -34,11 +32,9 @@ if [ -x "$(which python3)" ]; then
     ./py3/bin/pip install repoze.workflow==1.1
     ./py3/bin/pip install https://github.com/conestack/node/archive/master.zip
     ./py3/bin/pip install https://github.com/conestack/node.ext.ugm/archive/master.zip
-    ./py3/bin/pip install https://github.com/conestack/treibstoff/archive/master.zip
     ./py3/bin/pip install https://github.com/conestack/yafowil/archive/master.zip
-    ./py3/bin/pip install https://github.com/conestack/yafowil.bootstrap/archive/2.0.zip
     ./py3/bin/pip install https://github.com/conestack/cone.tile/archive/master.zip
-    ./py3/bin/pip install https://github.com/conestack/cone.app/archive/webresource.zip
-    ./py3/bin/pip install https://github.com/conestack/cone.ugm/archive/webresource.zip
+    ./py3/bin/pip install https://github.com/conestack/cone.app/archive/master.zip
+    ./py3/bin/pip install https://github.com/conestack/cone.ugm/archive/master.zip
     ./py3/bin/pip install -e .[test]
 fi
