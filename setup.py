e9d2ab5dd5eda63c8bd262c332ce34688fcb12b3
Robert Niederreiter <rnix@squarewave.at>
2024-02-12 11:15:19 +0100

    Back to development: 0.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(name):
         return f.read()
 
 
-version = '0.8'
+version = '0.9.dev0'
 shortdesc = 'SQLAlchemy integration for cone.app'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
